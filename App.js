81e1ed55c6fa652558771e0f24abe15789e1a4b6
flowkater <chjw102@gmail.com>
2016-10-10 20:07:35 +0900

    basic component stateless const

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return (
-      <div>History</div>
-    );
+    return <h1>Hello World</h1>;
   }
 }
+
+// const App = () => <h1>Hello flowkater</h1>
 
 export default App;
