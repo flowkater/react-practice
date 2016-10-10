d0864850c25cf1cd06d8fbd6ced8664af57d5c5b
flowkater <chjw102@gmail.com>
2016-10-10 21:32:24 +0900

    mount lifecycle practice

--- App.js
+++ App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends Component {
   constructor(){
     super();
-    this.state = {txt: ''};
+    this.state = {
+      red: 128,
+      green: 128,
+      blue: 128,
+      val: 0
+    };
     this.update = this.update.bind(this);
+    this.clickUpdate = this.clickUpdate.bind(this);
+  }
+  clickUpdate(){
+    this.setState({val: this.state.val + 1});
   }
   update(e){
-    this.setState({txt: e.target.value});
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+    });
+  }
+  componentWillMount() {
+    console.log('mounting!');
+  }
+  componentDidMount() {
+    console.log('mounted!');
+  }
+  componentWillUnmount() {
+    console.log('unmounted!');
   }
   render() {
+    console.log('rendering!');
     return(
       <div>
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
-        <Widget txt={this.state.txt} update={this.update} />
+        <Slider ref="red" update={this.update} />
+        {this.state.red}
+        <br />
+        <Slider ref="green" update={this.update} />
+        {this.state.green}
+        <br />
+        <Slider ref="blue" update={this.update} />
+        {this.state.blue}
+        <br />
+        <Button clickUpdate={this.clickUpdate} val={this.state.val}>I <Heart /> react</Button>
       </div>
     )
   }
@@ -28,20 +54,51 @@ class App extends Component {
 class Slider extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <input ref="inp" type="range"
+          min="0"
+          max="255"
+          onChange={this.props.update} />
+      </div>
+
+    );
+  }
+}
+
+class Wrapper extends Component {
+  constructor(){
+    super();
+  }
+  mount(){
+    ReactDOM.render(<App />,document.getElementById('a'));
+  }
+  unmount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'));
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>Unmount</button>
+        <div id="a"></div>
+      </div>
     );
   }
 }
 
 
-const Widget = (props) => {
-  return(
-    <div>
-      <input type="text"
-        onChange={props.update} />
-      <h1>{props.txt}</h1>
-    </div>
-  )
-}
+const Button = (props) => <button onClick={props.clickUpdate}>{props.children} {props.val}</button>;
 
-export default App;
+const Heart = () => <span className="glyphicon glyphicon-heart"></span>
+
+// const Widget = (props) => {
+//   return(
+//     <div>
+//       <input type="text"
+//         onChange={props.update} />
+//       <h1>{props.txt}</h1>
+//     </div>
+//   )
+// }
+
+export default Wrapper;
